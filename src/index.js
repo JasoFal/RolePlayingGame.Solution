4d0d0b6a801fd2dd402d486d6e96f3a3cbe9f457
Jason Falk <jasonfalk9@gmail.com>
2024-08-06 11:31:43 -0700

    Add functionality to create a character on button press.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import "./css/styles.css";
+
 const canCast = (state) => ({
   cast: (spell, dmgNum) => {
     state.mana--;
@@ -43,4 +45,38 @@ export const monster = (name, hp) => {
   }
   return Object.assign(state, monsterAttack());
 }
+
+function controlPlayerUiEle() {
+  document.getElementById("char-info").classList.remove("hidden");
+  document.getElementById("control-buttons").classList.remove("hidden");
+  document.getElementById("char-select").classList.add("hidden");
+}
+
+window.onload = function() {
+  document.getElementById("mage-select").onclick = function() {
+    const newMage = mage("Mage");
+    controlPlayerUiEle();
+    document.getElementById("char-name").innerText = `${newMage.name}`;
+    document.getElementById("char-hp").innerText = `${newMage.health}`;
+    document.getElementById("char-resource").innerText = `${newMage.mana}`;
+    document.getElementById("char-select").classList.add("hidden");
+  };
+
+  document.getElementById("fighter-select").onclick = function() {
+    const newFighter = fighter("Fighter");
+    controlPlayerUiEle();
+    document.getElementById("char-info").classList.remove("hidden");
+    document.getElementById("control-buttons").classList.remove("hidden");
+    document.getElementById("char-name").innerText = `${newFighter.name}`;
+    document.getElementById("char-hp").innerText = `${newFighter.health}`;
+    document.getElementById("char-resource").innerText = `${newFighter.stamina}`;
+  };
+
+  // document.getElementById("monster-spawn").onclick = function() {
+  //   const newMonster = monster("Giant Enemy Spider");
+  //   document.getElementById("monster-name").innerText = `${newMonster.name}`;
+  //   document.getElementById("monster-hp").innerText = `${newMonster.health}`;
+  //   document.getElementById("monster-spawn").classList.remove("hidden");
+  // };
+};
 
