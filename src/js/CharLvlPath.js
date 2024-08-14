1ef014a1bbb7599aa8dccf5ac5050449d6db8e25
Jason Falk <jasonfalk9@gmail.com>
2024-08-14 09:13:38 -0700

    Added health and player resource to lvlUp function.

--- src/js/CharLvlPath.js
+++ src/js/CharLvlPath.js
@@ -1,6 +1,8 @@
 // Lvl up paths for characters
 export function mageLvlPath(char) {
   if (char.lvl >= 1 && char.lvl <= 100) {
+    char.health += 40;
+    char.mana += 50;
     char.int += 2;
     char.str += 1;
     char.end += 1;
@@ -11,6 +13,8 @@ export function mageLvlPath(char) {
 
 export function fighterLvlPath(char) {
   if (char.lvl >= 1 && char.lvl <= 100) {
+    char.health += 60;
+    char.stamina += 30;
     char.int += 1;
     char.str += 2;
     char.end += 1;
