24eb8931ef970585eb342f642c254a76ec447f4a
Jason Falk <jasonfalk9@gmail.com>
2024-08-20 15:10:12 -0700

    Increase max health and resource on lvl up, also restores health and resource on lvl  up.

--- src/js/CharLvlPath.js
+++ src/js/CharLvlPath.js
@@ -1,11 +1,13 @@
 // Lvl up paths for characters
 export function mageLvlPath(char) {
   if (char.lvl >= 1 && char.lvl <= 100) {
-    char.health += 40;
-    char.mana += 50;
+    char.maxHealth += 18;
+    char.maxMana += 32;
     char.int += 2;
     char.str += 1;
     char.end += 1;
+    char.health = char.maxHealth;
+    char.mana = char.maxMana
   } else {
     return char.exp = 100;
   }
@@ -13,11 +15,13 @@ export function mageLvlPath(char) {
 
 export function fighterLvlPath(char) {
   if (char.lvl >= 1 && char.lvl <= 100) {
-    char.health += 60;
-    char.stamina += 30;
+    char.maxHealth += 22;
+    char.maxStamina += 12;
     char.int += 1;
     char.str += 2;
-    char.end += 1;
+    char.end += 2;
+    char.health = char.maxHealth;
+    char.stamina = char.maxStamina;
   } else {
     return char.exp = 100;
   }
