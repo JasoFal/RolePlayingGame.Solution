0546ac1d98158912a24b2527e0166322d2e85922
Jason Falk <jasonfalk9@gmail.com>
2024-08-21 23:15:20 -0700

    Add different enemy type and a monster spawning function.

--- src/js/monsterList.js
+++ src/js/monsterList.js
@@ -1,14 +1,57 @@
-const monsterAttack = () => ({
-  attack: (dmg) => {
-    return dmg;
-  }
-});
-
-export const monster = (name, hp, exp) => {
+const goblin = () => {
   let state = {
-    name,
-    health: hp,
-    expValue: exp
+    name: "Goblin",
+    nameFunction: "goblin",
+    hp: 30,
+    expValue: 20,
+    atk: 12,
+    init: 7
   };
-  return Object.assign(state, monsterAttack());
+  return Object.assign(state, goblinAttackList(state));
 };
+
+const goblinAttackList = (state) => {
+  const goblinAttacks = {
+    knifeAttack: {
+      atk: state.atk
+    }
+  };
+  return goblinAttacks;
+};
+
+const giantSpider = () => {
+  let state = {
+    name: "Giant Spider",
+    nameFunction: "giantspider",
+    hp: 40,
+    expValue: 12,
+    atk: 5,
+    init: 5
+  };
+  return Object.assign(state, giantSpiderAttackList(state));
+};
+
+const giantSpiderAttackList = (state) => {
+  const giantSpiderAttacks = {
+    bite: {
+      atk: state.atk
+    }
+  };
+  return giantSpiderAttacks;
+};
+
+export function enemySpawner(playerLvl) {
+  const rngRoll = Math.floor(Math.random() * 10);
+  if (playerLvl <= 5) {
+    if (rngRoll < 5) {
+      const newGiantSpider = giantSpider();
+      return newGiantSpider;
+    } else {
+      const newGoblin = goblin();
+      return newGoblin;
+    }
+  } else {
+    const newGiantSpider = giantSpider();
+    return newGiantSpider;
+  }
+}
